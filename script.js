a117b126d2e5141da41d87c46b179959a8631731
devhomie <devhomie@proton.me>
2024-07-07 19:29:05 -0400

    updated index.html and modified the script.js
    The -cx- attribute is based on the index of the data. The first element
    should be at 50, the second at 100, and so on.
    
    The calculation -(i+i)*50- gives us the right values. Because the circles
    are all in a line, the -cy- attribute is just a constant value.
    
    Now when you reload the page you should see five circles in a line.
    
    Now that we're using D3's - join - method to create new SVG elements as
    needed to suit the data, there's no reason to create them in the -- HTML file.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 
 let numbers = [3, 2, 1, 2, 3];
 
-d3.select("svg").selectAll("circle").data(numbers).join("circle").attr("r", (d,i) => d * 5);
+d3.select("svg").selectAll("circle").data(numbers).join("circle").attr("cx", (d, i)=> (i+1)*50).attr("cy", 50).attr("r", (d,i) => d * 5);
