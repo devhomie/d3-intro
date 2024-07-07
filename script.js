dbcb81f15167a278d04feb4bf1ec2d8b81b70af2
devhomie <devhomie@proton.me>
2024-07-07 19:40:03 -0400

    added buttons to index.html and created a -update- function in script.js
    There's no functional change here - we're just creating an -updatep function to
    do the SVG updating for us, and then calling it.
    
    Notice that we're passing -data-, the function's parameter, to the -.data- method,
    rather than passing the -numbers array directly.
    
    Next we'll add the code for handling button clicks, which will insert a random
    floating-point number between 1 to 5 into the numbers array at the start or end
    or drop the last element in the array.

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
-let numbers = [3, 2, 1, 2, 3];
+let numbers = [3, 2, 1];
 
-d3.select("svg").selectAll("circle").data(numbers).join("circle").attr("cx", (d, i)=> (i+1)*50).attr("cy", 50).attr("r", (d,i) => d * 5);
+function update(data){
+d3.select("svg").selectAll("circle").data(data).join("circle").attr("cx", (d, i)=> (i+1)*50).attr("cy", 50).attr("r", (d,i) => d * 5);
+  
+}
+
+update(numbers)
+
