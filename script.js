64f112e763d41b17d5eed549bf72ca5d790893ee
devhomie <devhomie@proton.me>
2024-07-07 22:17:43 -0400

    updated the -.join- method
    In this updated code, we've moved all the transitions into the individual
    [ enter ], [ update ], and [ exit ] function.
    
    Instead of having a single -transition- call for all the elements. The -enter-
    function appends a -circle- element and then immediately sets its position
    ( the -cx- and -cy- attributes ), but not its radius.
    
    Once the position is set, we use the -transition- method to animate the radius
    from zero ( the default value ) to the value calculated from the datum.
    
    The order here is important: anything that comes in the chain before the call
    to -transition- will happen immediately, and the change in size ( from zero
    to the desired radius ) will animate.
    
    This will agruably look more natural than the previous version, where all three
    attributes animated in from zero, leading to the circles flying from the corner.
    
    The -update- function has to animate only the [cx] attribute of the circle
    to slide it to its updated position.
    
    All other attributes should be unchanged for existing elements.
    
    Finally, the -exit- function animates the radius of the circle back to zero
    before removing it.
    
    If -remove- is called after a call to -transition- as it is here,
    the actual element removal won't take place until after the animations
    have completed.
    
    When you reload the page, you should see the new and improved animations:
    
    - new elements expan in at the appropriate position and removed
      elements shrink away.

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
-
 let numbers = [3, 2, 1];
 
-function update(data){
-d3.select("svg").selectAll("circle").data(data, d=>d).join(enter => enter.append("circle"), update => update, exit => exit.remove()).transition().duration(500).attr("cx", (d, i)=> (i+1)*50).attr("cy", 50).attr("r", (d,i) => d * 5);
-  
+function update(data) {
+  d3.select("svg")
+    .selectAll("circle")
+    .data(data, (d) => d)
+    .join(
+      (enter) =>
+        enter
+          .append("circle")
+          .attr("cx", (d, i) => (i + 1) * 50)
+          .attr("cy", 50)
+          .transition()
+          .attr("r", (d, i) => d * 5),
+      (update) =>
+        update
+          .transition()
+          .duration(500)
+          .attr("cx", (d, i) => (i + 1) * 50),
+      (exit) => exit.transition().duration(500).attr("r", 0).remove()
+    )
+    .transition()
+    .duration(500)
+    .attr("cx", (d, i) => (i + 1) * 50)
+    .attr("cy", 50)
+    .attr("r", (d, i) => d * 5);
 }
 
-update(numbers)
+update(numbers);
 
-function getRandomNumber(){
-  return 1+ Math.random() * 4;
+function getRandomNumber() {
+  return 1 + Math.random() * 4;
 }
 
-d3.select("#append").on("click", () =>{
+d3.select("#append").on("click", () => {
   numbers.push(getRandomNumber());
   update(numbers);
 });
 
-d3.select("#prepend").on("click", ()=>{
+d3.select("#prepend").on("click", () => {
   numbers.unshift(getRandomNumber());
   update(numbers);
-})
+});
 
-d3.select("#drop").on("click", ()=>{
+d3.select("#drop").on("click", () => {
   numbers.pop();
   update(numbers);
 });
